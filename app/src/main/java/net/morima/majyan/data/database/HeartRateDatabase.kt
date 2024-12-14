613f2607b949518893a32d5392a42fd55ffcf7b9
moridokin <mayumi24319@icloud.com>
2024-12-14 12:22:44 +0900

    DAO（データアクセスオブジェクト）の作成 データベースクラスの作成 ViewModelでデータベースを操作

--- app/src/main/java/net/morima/majyan/data/database/HeartRateDatabase.kt
+++ app/src/main/java/net/morima/majyan/data/database/HeartRateDatabase.kt
@@ -1,4 +1,28 @@
 package net.morima.majyan.data.database
 
-class HeartRateDatabase {
+import android.content.Context
+import androidx.room.Database
+import androidx.room.Room
+import androidx.room.RoomDatabase
+
+@Database(entities = [HeartRate::class], version = 1, exportSchema = false)
+abstract class HeartRateDatabase : RoomDatabase() {
+    abstract fun heartRateDao(): HeartRateDao
+
+    companion object {
+        @Volatile
+        private var INSTANCE: HeartRateDatabase? = null
+
+        fun getDatabase(context: Context): HeartRateDatabase {
+            return INSTANCE ?: synchronized(this) {
+                val instance = Room.databaseBuilder(
+                    context.applicationContext,
+                    HeartRateDatabase::class.java,
+                    "heart_rate_database"
+                ).build()
+                INSTANCE = instance
+                instance
+            }
+        }
+    }
 }
